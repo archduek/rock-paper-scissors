df237d7d37319cc709f44954e087aca202caa816
archduek <124202529+archduek@users.noreply.github.com>
2023-03-04 21:49:12 -0500

    Rewrote the whole program, working as intended

--- file.js
+++ file.js
@@ -9,28 +9,27 @@
 // paper > rock 
 // scissors > paper 
 
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 4) + 1;
-    if (choice === 1) {
+playOptions = [0, 1, 2];
+
+function getComputerChoice(items) {
+    let choice = items[Math.floor(Math.random()*items.length)];
+    if (choice === 0) {
         return "Rock";
-    } else if (choice === 2) {
+    } else if (choice === 1) {
         return "Paper";
-    } else if (choice === 3) {
+    } else if (choice === 2) {
         return "Scissors";
     }
 }
 
-let computerChoice = getComputerChoice();
-
 function playRound(computerChoice, playerChoice) {
     if (computerChoice == playerChoice) {
         return "Draw";
-    }
-    else if (computerChoice == "Rock") {
+    } else if (computerChoice == "Rock") {
         if (playerChoice == "Scissors") {
             return "Computer wins";
         } else {
-            return "Human wins";
+            return "Player wins";
         }
     } else if (computerChoice == "Paper") {
         if (playerChoice == "Rock") {
@@ -42,33 +41,18 @@ function playRound(computerChoice, playerChoice) {
         if (playerChoice == "Paper") {
             return "Computer wins";
         } else {
-            return "Human wins";
+            return "Player wins";
         }
-    } 
+    }
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) { 
-        let playerInput = String(prompt("Rock, Paper, Scissors?"));
-        let playerChoice = (playerInput.charAt(0).toUpperCase() + playerInput.substring(1).toLowerCase());
-        getComputerChoice;
+    for (let i = 0; i < 5; i++) {
+        let input = String(prompt("Rock, Paper, Scissors?"));
+        let playerChoice = input.charAt(0).toUpperCase() + input.substring(1).toLowerCase();
+        let computerChoice = getComputerChoice(playOptions);
         console.log(playRound(computerChoice, playerChoice));
-        
-        score;
-    } 
-}
-
-function score() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    if (game == "Human wins") {
-        playerScore++;
-    } else if (game == "Computer wins") {
-        computerScore++;
     }
-
-    console.log(`Player Score: ${playerScore}, Computer Score ${computerScore}`);
 }
 
 console.log(game());
